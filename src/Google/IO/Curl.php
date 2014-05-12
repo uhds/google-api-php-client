481d5d5cbb31f41d851226118fdd6f853559b0be
Gordon Stratton <gordon.stratton@oregonstate.edu>
2014-05-12 21:16:25 +0000

    Work around proxy header inclusion in HTTP responses when using cURL
    This is not a robust or correct fix - one should be provided upstream
    eventually.
    
    See my comment:
    
    https://github.com/google/google-api-php-client/pull/110#issuecomment-42888167

--- src/Google/IO/Curl.php
+++ src/Google/IO/Curl.php
@@ -82,8 +82,11 @@ class Google_IO_Curl extends Google_IO_Abstract
     }
     $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
 
-    $responseBody = substr($response, $headerSize);
-    $responseHeaderString = substr($response, 0, $headerSize);
+    if (stripos($response, "HTTP/1.0 200 Connection established\r\n\r\n") !== false) {
+        $response = str_ireplace("HTTP/1.0 200 Connection established\r\n\r\n", '', $response);
+    }
+
+    list($responseHeaderString, $responseBody) = explode("\r\n\r\n", $response, 2);
     $responseHeaders = $this->getHttpResponseHeaders($responseHeaderString);
     $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
